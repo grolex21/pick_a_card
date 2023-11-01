b75c65d8a3c7a365047e4e9c28cdbf5af873380b
Alexander Grigorev <grolex@mail.ru>
2023-11-01 19:53:20 +0300

    change suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -13,4 +13,4 @@ values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['♦', '♥', '♠', '♣']
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
